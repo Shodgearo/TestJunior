59e5856beca8799eae2f850b38db0e2af3699e06
Shodgearo <Shodkvest@gmail.com>
2018-02-20 18:23:10 +0600

    Порверка на создание потоков

--- src/Child.java
+++ src/Child.java
@@ -5,12 +5,12 @@ public class Child implements Runnable {
 
     public Child(String name) {
         this.name = name;
-        t = new Thread();
+        t = new Thread(this);
         t.start();
     }
 
     @Override
     public void run() {
-
+        System.out.println(name + " starting and closing");
     }
 }
